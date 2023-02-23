ca004c15765ae6017bcc33c9d7546c7b6dee5b6b
Sam <sammynarduzzi@gmail.com>
2023-02-22 19:16:25 -0800

    created Show component with story in Appointment folder

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 import "index.scss";
 
@@ -146,3 +147,11 @@ storiesOf("Appointment", module)
   ))
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show 
+      student="Lydua Miller-Jones"
+      interviewer={interviewer.name}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ));
