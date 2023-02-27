5f130602843ac0493521463b6b430b736e5b5cc5
Sam <sammynarduzzi@gmail.com>
2023-02-26 21:53:20 -0800

    useVisualMode and testing

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, prev.length -1), newMode])
+    } else {
+      setHistory((prev) => [...prev, newMode])
+    }
+  }
+
+  function back() {
+    if (history.length === 1) {
+      setMode(initial);
+    } else {
+      setMode(history[history.length - 2]);
+      setHistory(history.slice(0, -1));
+    }
+  }
+
+  return { mode, transition, back };
+}
