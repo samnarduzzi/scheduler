6ca24b28eac9ec795d0f29362e3ecfe0c9dc1575
Sam <sammynarduzzi@gmail.com>
2023-02-28 13:05:41 -0800

    implement updateSpots function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -40,7 +40,7 @@ export default function useApplicationData() {
     };
     return axios.put(`/api/appointments/${id}`, { interview })
       .then(response => {
-        setState({ ...state, appointments });
+        setState({ ...state, appointments, days: updateSpots(id, appointments) });
       });
   }
 
@@ -55,9 +55,34 @@ export default function useApplicationData() {
     };
     return axios.delete(`/api/appointments/${id}`)
       .then(response => {
-        setState({ ...state, appointments });
+        setState({ ...state, appointments, days: updateSpots(id, appointments) });
       });
   }
+
+  const updateSpots = (appointmentId, appointments) => {
+    let day = null;
+    for (let i = 0; i < state.days.length; i++) {
+      if (state.days[i].appointments.includes(appointmentId)) {
+        day = state.days[i];
+        break;
+      }
+    }
+
+    let spots = 0;
+    for (let i = 0; i < day.appointments.length; i++) {
+      const appointment = appointments[day.appointments[i]];
+      if (appointment.interview === null) {
+        spots++;
+      }
+    }
+
+    return state.days.map((day) => {
+      if (day.appointments.includes(appointmentId)) {
+        return { ...day, spots: spots };
+      }
+      return day;
+    });
+  };
 
   return { state, setDay, bookInterview, cancelInterview };
 }
