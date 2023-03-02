d8ae66e5745edd8745b0d09a8eb119423d19958b
Sam <sammynarduzzi@gmail.com>
2023-03-01 17:10:30 -0800

    remove comments

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -45,39 +45,29 @@ describe("Application", () => {
   });
 
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-    // 1. Render the Application.
+
     const { container } = render(<Application />);
-    // 2. Wait until the text "Archie Cohen" is displayed.
+
     await waitForElement(() => getByText(container, "Archie Cohen"));
-    // 3. Click the "Delete" button on the booked appointment.
+
     const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen"));
 
     fireEvent.click(queryByAltText(appointment, "Delete"));
-    // 4. Check that the confirmation message is shown.
+
     expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
-    // 5. Click the "Confirm" button on the confirmation.
+
     fireEvent.click(queryByText(appointment, "Confirm"));
-    // 6. Check that the element with the text "Deleting" is displayed.
+
     expect(getByText(appointment, "Deleting")).toBeInTheDocument();
-    // 7. Wait until the element with the "Add" button is displayed.
+
     await waitForElement(() => getByAltText(appointment, "Add"));
-    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
     const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
 
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
-    // 1. Render the Application.
-    // 2. Wait until the text "Archie Cohen" is displayed.
-    // 3. Click the "Edit" button on the booked appointment.
-    // 4. Check that the edit form is shown and that the input contains "Archie Cohen".
-    // 5. Enter the new name of the student "Lydia Miller-Jones"
-    // 6. Change the interviewer to "Sylvia Palmer"
-    // 7. Click the "Save" button on the confirmation.
-    // 8. Check that the element with the text "Saving" is displayed.
-    // 9. Wait until the element with the student name is displayed.
-    // 10. Check that the DayListItem with the text "Monday" also has the text "1 spots remaining".
     const { container } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
